579ae99ff34447757a2d65cb288527df35fbfc82
Ankit Kumar <krankit2229@gmail.com>
2026-09-15 16:22:47 +0530

    Update MeetFlow frontend

--- src/environment.js
+++ src/environment.js
@@ -1,16 +1,7 @@
-// let IS_PROD = true;
-// const server = IS_PROD ?
-//     "https://apnacollegebackend.onrender.com" :
-
-//     "http://localhost:8000"
-
-
-// export default server;
-
-let IS_PROD = false;
+let IS_PROD = true;
 
 const server = IS_PROD
-    ? "https://apnacollegebackend.onrender.com"
+    ? "https://meetflow-backend-issc.onrender.com"
     : "http://localhost:8000";
 
 export default server;
